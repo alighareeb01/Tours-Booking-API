71140a15fc2cef724e24f878dae93e30fb0226c2
alighareeb01 <ghareeb874@gmail.com>
2026-06-30 18:19:54 +0300

    Added Bookin Model/routes/handler functions

--- 2-natours/controllers/bookingController.js
+++ 2-natours/controllers/bookingController.js
@@ -2,6 +2,8 @@ import { catchAsync } from "../utils/catchAsync.js";
 import Stripe from "stripe";
 import dotenv from "dotenv";
 import { Tour } from "../models/tourModel.js";
+import handlerFactory from "./handlerFactory.js";
+import { Booking } from "../models/bookingModel.js";
 
 dotenv.config({ path: "./config.env" });
 const stipreCliet = new Stripe(process.env.STRIPE_SECRETKEY);
@@ -11,7 +13,10 @@ export const getCheckoutSession = catchAsync(async (req, res, nex) => {
 
   const session = await stipreCliet.checkout.sessions.create({
     payment_method_types: ["card"],
-    success_url: `${req.protocol}://${req.get("host")}/`,
+    // success_url: `${req.protocol}://${req.get("host")}/`,
+    success_url: `${req.protocol}://${req.get("host")}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get("host")}/`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -37,3 +42,18 @@ export const getCheckoutSession = catchAsync(async (req, res, nex) => {
     session,
   });
 });
+
+export const createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split("?")[0]);
+});
+
+export const createBooking = handlerFactory.createOne(Booking);
+export const getBooking = handlerFactory.getOne(Booking);
+export const getAllBookings = handlerFactory.getAll(Booking);
+export const updateBooking = handlerFactory.updateOne(Booking);
+export const deleteBooking = handlerFactory.deleteOne(Booking);
--- 2-natours/routes/bookingRoutes.js
+++ 2-natours/routes/bookingRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { protect } from "../controllers/authController.js";
-import { getCheckoutSession } from "../controllers/bookingController.js";
+import { protect, restrictTo } from "../controllers/authController.js";
+
+import {
+  createBooking,
+  deleteBooking,
+  getAllBookings,
+  getBooking,
+  getCheckoutSession,
+  updateBooking,
+} from "../controllers/bookingController.js";
 
 const router = express.Router();
 
-router.get("/checkout-session/:tourId", protect, getCheckoutSession);
+router.use(protect);
+
+router.get("/checkout-session/:tourId", getCheckoutSession);
+
+router.use(restrictTo("admin", "lead-guide"));
+
+router.route("/").get(getAllBookings).post(createBooking);
+
+router.route("/:id").get(getBooking).patch(updateBooking).delete(deleteBooking);
 
 export default router;
